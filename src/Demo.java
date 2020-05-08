c1ca82d5aee106bab31ea5d8b713375dbc39a863
vdelll <valentin.delaunay1@gmail.com>
2020-05-08 06:00:05 +0200

    Types d'injections SQL possibles

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,8 +9,12 @@ import java.util.Properties;
 
 public class Demo {
 
+	// Permet de lire des chaines de caractère en faisant readline sur la console
+	private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void main(String[] args) throws Exception {
 
+		// Chargement du fichier de configuration
 		Properties props = new Properties();
 
 		try (FileInputStream fis = new FileInputStream("conf.properties")) {
@@ -21,11 +27,54 @@ public class Demo {
 		Class.forName(props.getProperty("jdbc.driver.class"));
 
 		String url = props.getProperty("jdbc.url");
-		String login = props.getProperty("jdbc.login");
-		String password = props.getProperty("jdbc.password");
+		String dbLogin = props.getProperty("jdbc.login");
+		String dbPassword = props.getProperty("jdbc.password");
 
 		// Création d'une connexion
-		try (Connection connection = DriverManager.getConnection(url, login, password)) {
+		try (Connection connection = DriverManager.getConnection(url, dbLogin, dbPassword)) {
+
+			String readedLogin = "";
+			System.out.println("---------------- WebStoreTest v1.0 ----------------");
+
+			while (true) {
+
+				// Champs de saisie des informations de connexion
+				System.out.print("Login : ");
+				String login = keyboard.readLine();
+				System.out.print("Password : ");
+				String password = keyboard.readLine();
+
+				// Select avec les informations récupérées
+				String strSql = "SELECT * FROM T_Users WHERE login= '" + login + "' AND password='" + password + "'";
+
+				try (Statement statement = connection.createStatement();
+						ResultSet resultSet = statement.executeQuery(strSql)) {
+
+					// Si l'utilisateur n'existe pas, le next renvoie false
+					if (resultSet.next()) {
+
+						// Ajoute une connexion si l'utilisateur se connecte
+						strSql = "UPDATE T_Users SET connectionNumber=connectionNumber+1 WHERE idUser = "
+								+ resultSet.getInt("idUser");
+
+						try (Statement stUpdate = connection.createStatement()) {
+
+							stUpdate.executeUpdate(strSql);
+
+						}
+
+						// Sort de la boucle si l'utilisateur est connecté
+						readedLogin = resultSet.getString("login");
+						break;
+					}
+
+					System.out.println("Wrong password !");
+
+				}
+
+			}
+			
+			System.out.println("Welcome " + readedLogin + " !");
 
 //			String strSql = "INSERT INTO T_Users (idUser, login, password, connectionNumber) "
 //					+ "VALUES (6, 'Bourne', 'Jason', 8);";
@@ -35,26 +84,26 @@ public class Demo {
 //				statement.executeUpdate(strSql);
 //				
 //			}
-
-			String strSql = "SELECT * FROM T_Users;";
-
-			// Création du statement et du resultset dans le même try-with-ressouces
-			try (Statement statement = connection.createStatement();
-					ResultSet resultSet = statement.executeQuery(strSql)) {
-
-				while (resultSet.next()) {
-
-					// Récupération des éléments
-					int rsIdUser = resultSet.getInt(1);
-					String rsLogin = resultSet.getString(2);
-					String rsPassword = resultSet.getString("password");
-					int rsConnectionNumber = resultSet.getInt("connectionNumber");
-
-					System.out.printf("%d: %s %s - %d\n", rsIdUser, rsLogin, rsPassword, rsConnectionNumber);
-
-				}
-
-			}
+//
+//			String strSql = "SELECT * FROM T_Users;";
+//
+//			// Création du statement et du resultset dans le même try-with-ressouces
+//			try (Statement statement = connection.createStatement();
+//					ResultSet resultSet = statement.executeQuery(strSql)) {
+//
+//				while (resultSet.next()) {
+//
+//					// Récupération des éléments
+//					int rsIdUser = resultSet.getInt(1);
+//					String rsLogin = resultSet.getString(2);
+//					String rsPassword = resultSet.getString("password");
+//					int rsConnectionNumber = resultSet.getInt("connectionNumber");
+//
+//					System.out.printf("%d: %s %s - %d\n", rsIdUser, rsLogin, rsPassword, rsConnectionNumber);
+//
+//				}
+//
+//			}
 
 		}
 
